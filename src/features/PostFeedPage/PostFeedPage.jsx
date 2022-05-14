eca096c7e3cc01c8364fd7cf5f4e4b71dfcb4559
Rohan Mondal <mondalrohan108@gmail.com>
2022-05-14 20:07:52 +0530

    removed create post from explore

--- src/features/PostFeedPage/PostFeedPage.jsx
+++ src/features/PostFeedPage/PostFeedPage.jsx
@@ -114,127 +114,130 @@ export const PostFeedPage = () => {
           {/* news feed */}
           <div className='flex flex-col  w-2/5 md:w-4/5 sm:w-full  gap-4 '>
             {/* create post section */}
-            <div className='flex flex-col bg-nav-background rounded-lg drop-shadow-2xl divide-y divide-blue-200'>
-              <div className='p-4'>
-                <h1 className='text-xl'>Create Post</h1>
-              </div>
-              <div className='p-4'>
-                <div className='flex flex-col gap-4 grow'>
-                  {/** enter text section */}
-                  <div className='flex items-center gap-4 grow'>
-                    <img
-                      className='h-14 w-14 rounded-full object-cover'
-                      src={user?.pic}
-                      alt='profile-img'
-                    />
-                    <input
-                      className='grow focus:outline-none font-light text-txt-secondary-color'
-                      placeholder='Write something here'
-                      value={postInputForm.content}
-                      onChange={(e) =>
-                        setPostInputForm({
-                          ...postInputForm,
-                          content: e.target.value,
-                        })
-                      }
-                      type='text'
-                    />
-                  </div>
-                  {postInputForm.pic ? (
-                    <div className='relative'>
-                      <img src={postInputForm.pic} alt='post pic' />
-                      <i
-                        onClick={() =>
-                          setPostInputForm({ ...postInputForm, pic: '' })
-                        }
-                        className='absolute top-1 right-1 text-4xl text-txt-hover-color cursor-pointer fas fa-times-circle'
-                      ></i>
-                    </div>
-                  ) : null}
-                  <hr className='font-extralight text-secondary' />
-                  <ul className='flex gap-4 font-light items-center'>
-                    <li className='relative flex items-center gap-3 bg-secondary-background py-2 px-3 rounded-md cursor-pointer'>
+            {!location.pathname.includes('/explore') ? (
+              <div className='flex flex-col bg-nav-background rounded-lg drop-shadow-2xl divide-y divide-blue-200'>
+                <div className='p-4'>
+                  <h1 className='text-xl'>Create Post</h1>
+                </div>
+                <div className='p-4'>
+                  <div className='flex flex-col gap-4 grow'>
+                    {/** enter text section */}
+                    <div className='flex items-center gap-4 grow'>
                       <img
-                        className='h-6 w-6'
-                        src='https://res.cloudinary.com/donqbxlnc/image/upload/v1650190023/07_dffvl5.png'
-                        alt='phot'
+                        className='h-14 w-14 rounded-full object-cover'
+                        src={user?.pic}
+                        alt='profile-img'
                       />
-                      <p className='text-primary text-sm font-semibold'>
-                        Photo/GIF
-                      </p>
                       <input
-                        className='cursor-pointer absolute w-28 opacity-0'
-                        accept='image/apng, image/avif, image/gif, image/jpeg, image/png, image/svg+xml, image/jpg,image/webp'
-                        type='file'
-                        onChange={onFileChange}
-                      />
-                    </li>
-                    <li
-                      onClick={(e) => {
-                        setShowEmojis(true);
-                        if (
-                          e.target.childNodes.length === 1 &&
-                          e.target.innerText !== 'Emojis'
-                        ) {
+                        className='grow focus:outline-none font-light text-txt-secondary-color'
+                        placeholder='Write something here'
+                        value={postInputForm.content}
+                        onChange={(e) =>
                           setPostInputForm({
                             ...postInputForm,
-                            content: postInputForm.content + e.target.innerText,
-                          });
-
-                          setShowEmojis(false);
+                            content: e.target.value,
+                          })
                         }
-                      }}
-                      className='relative flex items-center gap-3 bg-secondary-background py-2 px-3 rounded-md cursor-pointer'
-                    >
-                      <img
-                        className='h-6 w-6'
-                        src='https://res.cloudinary.com/donqbxlnc/image/upload/v1652278871/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_grande_tz0jya.png'
-                        alt='emojis'
+                        type='text'
                       />
-                      <p className='text-primary text-sm font-semibold'>
-                        Emojis
-                      </p>
-                      {showEmojis ? (
-                        <div
-                          ref={emojiContainerRef}
-                          className='absolute w-48 p-4 flex flex-wrap justify-center items-center gap-1 rounded-lg  bg-secondary-background '
-                        >
-                          {emojiLib.map((el) => {
-                            return (
-                              <span
-                                key={el}
-                                className='cursor-pointer text-2xl'
-                              >
-                                {el}
-                              </span>
-                            );
-                          })}
-                        </div>
-                      ) : null}
-                    </li>
-                  </ul>
+                    </div>
+                    {postInputForm.pic ? (
+                      <div className='relative'>
+                        <img src={postInputForm.pic} alt='post pic' />
+                        <i
+                          onClick={() =>
+                            setPostInputForm({ ...postInputForm, pic: '' })
+                          }
+                          className='absolute top-1 right-1 text-4xl text-txt-hover-color cursor-pointer fas fa-times-circle'
+                        ></i>
+                      </div>
+                    ) : null}
+                    <hr className='font-extralight text-secondary' />
+                    <ul className='flex gap-4 font-light items-center'>
+                      <li className='relative flex items-center gap-3 bg-secondary-background py-2 px-3 rounded-md cursor-pointer'>
+                        <img
+                          className='h-6 w-6'
+                          src='https://res.cloudinary.com/donqbxlnc/image/upload/v1650190023/07_dffvl5.png'
+                          alt='phot'
+                        />
+                        <p className='text-primary text-sm font-semibold'>
+                          Photo/GIF
+                        </p>
+                        <input
+                          className='cursor-pointer absolute w-28 opacity-0'
+                          accept='image/apng, image/avif, image/gif, image/jpeg, image/png, image/svg+xml, image/jpg,image/webp'
+                          type='file'
+                          onChange={onFileChange}
+                        />
+                      </li>
+                      <li
+                        onClick={(e) => {
+                          setShowEmojis(true);
+                          if (
+                            e.target.childNodes.length === 1 &&
+                            e.target.innerText !== 'Emojis'
+                          ) {
+                            setPostInputForm({
+                              ...postInputForm,
+                              content:
+                                postInputForm.content + e.target.innerText,
+                            });
+
+                            setShowEmojis(false);
+                          }
+                        }}
+                        className='relative flex items-center gap-3 bg-secondary-background py-2 px-3 rounded-md cursor-pointer'
+                      >
+                        <img
+                          className='h-6 w-6'
+                          src='https://res.cloudinary.com/donqbxlnc/image/upload/v1652278871/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_grande_tz0jya.png'
+                          alt='emojis'
+                        />
+                        <p className='text-primary text-sm font-semibold'>
+                          Emojis
+                        </p>
+                        {showEmojis ? (
+                          <div
+                            ref={emojiContainerRef}
+                            className='absolute w-48 p-4 flex flex-wrap justify-center items-center gap-1 rounded-lg  bg-secondary-background '
+                          >
+                            {emojiLib.map((el) => {
+                              return (
+                                <span
+                                  key={el}
+                                  className='cursor-pointer text-2xl'
+                                >
+                                  {el}
+                                </span>
+                              );
+                            })}
+                          </div>
+                        ) : null}
+                      </li>
+                    </ul>
+                  </div>
                 </div>
+                <button
+                  onClick={() => {
+                    dispatch(
+                      addPost({
+                        postData: { ...postInputForm, userId: user._id },
+                        authToken: token,
+                      })
+                    );
+                    setPostInputForm({ content: '', pic: '' });
+                  }}
+                  disabled={postInputForm.content ? false : true}
+                  className={`mb-4 mx-4 p-2 bg-primary active:bg-blue-500 text-white rounded-lg ${
+                    postInputForm.content
+                      ? ''
+                      : 'cursor-not-allowed bg-txt-hover-color active:bg-txt-hover-color'
+                  }`}
+                >
+                  Post
+                </button>
               </div>
-              <button
-                onClick={() => {
-                  dispatch(
-                    addPost({
-                      postData: { ...postInputForm, userId: user._id },
-                      authToken: token,
-                    })
-                  );
-                  setPostInputForm({ content: '', pic: '' });
-                }}
-                disabled={postInputForm.content ? false : true}
-                className={`mb-4 mx-4 p-2 bg-primary active:bg-blue-500 text-white rounded-lg ${
-                  postInputForm.content
-                    ? ''
-                    : 'cursor-not-allowed bg-txt-hover-color active:bg-txt-hover-color'
-                }`}
-              >
-                Post
-              </button>
-            </div>
+            ) : null}
 
             <div className='flex justify-evenly items-center   bg-nav-background gap-10 rounded-lg drop-shadow-2xl  p-4 sm:p-3 '>
               <div
