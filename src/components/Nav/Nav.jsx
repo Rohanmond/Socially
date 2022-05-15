3411ce60a8dd5198fd76752f06953012a8867c28
Rohan Mondal <mondalrohan108@gmail.com>
2022-05-16 01:05:57 +0530

    fixed gap in search

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -135,7 +135,7 @@ export const Nav = () => {
                         navigate(`/profile/${user.userHandler}`);
                         setShowSearch(false);
                       }}
-                      className='px-4 pt-3 last-of-type:pb-3 cursor-pointer  flex justify-between items-center'
+                      className='px-4 pt-3 last-of-type:pb-3 cursor-pointer  flex gap-4 items-center'
                     >
                       <img
                         className='w-20 sm:w-16 sm:h-16 h-20 object-cover rounded-full'
